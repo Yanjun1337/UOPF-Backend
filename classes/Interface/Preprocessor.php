a915c2b73e7d6bb4baa4ef29819d43a221388eaa
Yanjun Deng <y_deng1@u.pacific.edu>
2025-11-06 09:35:49 +0800

    Create data embedding mechanism

--- classes/Interface/Preprocessor.php
+++ classes/Interface/Preprocessor.php
@@ -6,23 +6,36 @@ use UOPF\Model;
 use UOPF\Exception as SystemException;
 use UOPF\Model\User as UserModel;
 use UOPF\Model\Image as ImageModel;
+use UOPF\Interface\Embeddable\Entry as EmbeddableEntry;
 
 /**
  * Return Data Preprocessor
  */
 final class Preprocessor {
+    /**
+     * The embedding instructions.
+     */
+    public readonly array $embedding;
+
+    /**
+     * The stack of the current field path.
+     */
+    protected $stack = [];
+
     public function __construct(
         /**
          * The endpoint context.
          */
         public readonly Endpoint $context
-    ) {}
+    ) {
+        $this->embedding = $this->parseEmbeddingInstruction();
+    }
 
     public function preprocess(mixed $data): mixed {
         if (is_scalar($data) || is_null($data))
             return $data;
         elseif (is_object($data))
-            return $this->preprocessInstance($data);
+            return $this->preprocess($this->preprocessInstance($data));
         elseif (is_array($data))
             return $this->preprocessArray($data);
         else
@@ -31,6 +44,9 @@ final class Preprocessor {
 
     protected function preprocessInstance(object $data): mixed {
         switch (true) {
+            case $data instanceof Embeddable:
+                return $this->preprocessEmbeddable($data);
+
             case $data instanceof UserModel:
                 return $this->preprocessUser($data);
 
@@ -43,7 +59,31 @@ final class Preprocessor {
     }
 
     protected function preprocessArray(array $data): mixed {
-        static::throwUnprocessableException();
+        $preprocessed = [];
+
+        foreach ($data as $key => $value) {
+            $this->stack[] = $key;
+            $preprocessed[$key] = $this->preprocess($value);
+            array_pop($this->stack);
+        }
+
+        return $preprocessed;
+    }
+
+    protected function preprocessEmbeddable(Embeddable $data): mixed {
+        $path = str_replace('.[]', '[]', implode('.', $this->stack));
+        $embedded = in_array($path, $this->embedding, true);
+
+        switch (true) {
+            case $data instanceof EmbeddableEntry:
+                if ($embedded)
+                    return $data->getEntry();
+                else
+                    return $data->id;
+
+            default:
+                throw new SystemException('Unsupported type of embeddable value.');
+        }
     }
 
     protected function preprocessUser(UserModel $user): array {
@@ -96,7 +136,7 @@ final class Preprocessor {
         ];
 
         if (isset($image['user']))
-            $preprocessed['user'] = $image['user'];
+            $preprocessed['user'] = new EmbeddableEntry($image['user'], 'user');
 
         if ($this->context->isAdministrative() && isset($image['record']))
             $preprocessed['record'] = $image['record'];
@@ -112,6 +152,16 @@ final class Preprocessor {
             $preprocessed['raw'] = $entry[$field];
 
         return $preprocessed;
+    }
+
+    protected function parseEmbeddingInstruction(): array {
+        $header = $this->context->request->headers->get('X-API-Embed');
+
+        if (!isset($header))
+            return [];
+
+        $instruction = explode(',', $header);
+        return array_map('trim', $instruction);
     }
 
     protected static function throwUnprocessableException(): never {
